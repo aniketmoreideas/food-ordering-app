484e374a2a3e67a2584726ce81bce780ca398314
Aniket Sharma <aniket.sharma@moreideas.ae>
2024-02-28 12:09:55 +0400

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Header from "@/components/layouts/Header";
 import Hero from "@/components/layouts/Hero";
 import HomeMenu from "@/components/layouts/HomeMenu";
+import SectionHeaders from "@/components/layouts/SectionHeaders";
 
 export default function Home() {
   return (
@@ -8,6 +9,15 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu />
+      <section>
+        <div className="text-center my-8">
+          <SectionHeaders mainHeader={'About us'} subHeader={'Our Story'} />
+        </div>
+        <div className="max-w-xl mx-auto text-center text-gray-600 flex-col gap-4 flex">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto eos consequuntur quasi officia recusandae ipsa est enim tenetur consectetur, ratione, ipsam id perferendis. Sequi quidem quia commodi. Aliquid, officiis exercitationem.</p>
+          <p>Iusto eos consequuntur quasi officia recusandae ipsa est enim tenetur consectetur, ratione, ipsam id perferendis. Sequi quidem quia commodi. Aliquid, officiis exercitationem.</p>
+        </div>
+      </section>
     </>
   );
 }
--- src/components/layouts/HomeMenu.js
+++ src/components/layouts/HomeMenu.js
@@ -1,28 +1,28 @@
 import Image from "next/image";
+import MenuItem from "../menu/MenuItem";
+import SectionHeaders from "./SectionHeaders";
 export default function HomeMenu() {
   return (
     <section className="py-4">
-      <div className="absolute left-0 right-0 h-full">
+      <div className="absolute left-0 right-0">
         <div className="absolute left-0 ">
-          <Image
-            src={"/sallad1.png"}
-            width={"109"}
-            height={"189"}
-          />
+          <Image src={"/sallad1.png"} width={"109"} height={"189"} />
         </div>
         <div className="absolute -top-28 right-0">
-          <Image
-            src={"/sallad2.png"}
-            width={"107"}
-            height={"196"}
-          />
+          <Image src={"/sallad2.png"} width={"107"} height={"196"} />
         </div>
       </div>
-      <div className="text-center ">
-        <h3 className="leading-4 uppercase text-xl font-semibold text-gray-500">
-          Check Out
-        </h3>
-        <h2 className="text-4xl font-bold text-primary italic">Menu</h2>
+      <div className="text-center my-8">
+        <SectionHeaders subHeader={"Check Out"} mainHeader={"Menu"} />
+      </div>
+
+      <div className="grid grid-cols-3 gap-4">
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
       </div>
     </section>
   );
