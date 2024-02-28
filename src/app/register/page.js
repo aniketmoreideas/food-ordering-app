edc4c262df2b6249118ec525c4c90ed220034fd0
Aniket Sharma <aniket.sharma@moreideas.ae>
2024-02-28 17:26:57 +0400

    update

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,23 +1,49 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleFormSubmt(ev) {
+    ev.preventDefault();
+    fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-type": "application/json" },
+    });
+  }
+
   return (
     <section className="my-8 max-w-xs mx-auto">
       <h1 className="text-center text-primary text-3xl font-semibold">
         Register
       </h1>
-      <form action="">
-        <input type="email" name="email" id="email" placeholder="Email" />
+      <form onSubmit={handleFormSubmt}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(ev) => setEmail(ev.target.value)}
+          required={true}
+        />
         <input
           type="password"
           name="password"
-          id="password"
           placeholder="Password"
+          value={password}
+          required={true}
+          onChange={(ev) => setPassword(ev.target.value)}
         />
         <button type="submit">Register</button>
       </form>
-      <p className="text-center text-gray-500 py-3 text-sm italic">or login with Google</p>
+      <p className="text-center text-gray-500 py-3 text-sm italic">
+        or login with Google
+      </p>
       <button className="flex justify-center gap-2 items-center">
-        <Image src={"/google.png"} height={"16"} width={"16"} />
+        <Image src={"/google.png"} alt="google" height={"16"} width={"16"} />
         Login with Google
       </button>
     </section>
