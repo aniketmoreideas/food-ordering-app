a0b2b2a91f78e7f9074dd0f2561794570f07690a
Aniket Sharma <aniket.sharma@moreideas.ae>
2024-02-29 16:53:19 +0400

    register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,18 +1,32 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
 
-  function handleFormSubmt(ev) {
+  async function handleFormSubmt(ev) {
     ev.preventDefault();
-    fetch("/api/register", {
+    setCreatingUser(true);
+    setError(false);
+    setUserCreated(false);
+    const response = await fetch("/api/register", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-type": "application/json" },
     });
+    setCreatingUser(false);
+    // console.log(response);
+    if (response.ok) {
+      setUserCreated(true);
+    } else {
+      setError(true);
+    }
   }
 
   return (
@@ -20,6 +34,21 @@ export default function Register() {
       <h1 className="text-center text-primary text-3xl font-semibold">
         Register
       </h1>
+      {userCreated && (
+        <div className="text-sm text-center text-gray-800 my-3">
+          User created successfully <br />
+          Now you can{" "}
+          <Link href={"/login"} className="underline italic">
+            Login &raquo;
+          </Link>
+        </div>
+      )}
+      {error && (
+        <div className="text-sm text-center text-gray-800 my-3">
+          Error! Please try again!
+        </div>
+      )}
+
       <form onSubmit={handleFormSubmt}>
         <input
           type="email"
@@ -28,6 +57,7 @@ export default function Register() {
           value={email}
           onChange={(ev) => setEmail(ev.target.value)}
           required={true}
+          disabled={creatingUser}
         />
         <input
           type="password"
@@ -36,8 +66,11 @@ export default function Register() {
           value={password}
           required={true}
           onChange={(ev) => setPassword(ev.target.value)}
+          disabled={creatingUser}
         />
-        <button type="submit">Register</button>
+        <button type="submit" disabled={creatingUser}>
+          Register
+        </button>
       </form>
       <p className="text-center text-gray-500 py-3 text-sm italic">
         or login with Google
@@ -46,6 +79,10 @@ export default function Register() {
         <Image src={"/google.png"} alt="google" height={"16"} width={"16"} />
         Login with Google
       </button>
+
+      <div className="text-center my-4 text-sm text-gray-800">
+        Existing account? <Link href={"/login"} className="underline italic">Login &raquo;</Link>
+      </div>
     </section>
   );
 }
